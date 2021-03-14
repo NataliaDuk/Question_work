e88eeeb372b38251819eb5b6c957f363f64aae5b
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-03-14 23:08:56 +0300

    save mysql

--- main.php
+++ main.php
@@ -1,26 +1,25 @@
 <?php
-$bd=mysqli_connect('localhost', 'root', 'root', 'registration');
-if (isset($_POST['']))
-    if (isset($_POST['submit'])){
-        $username=mysqli_real_escape_string($bd, trim($_POST['username']));
-        $password1=mysqli_real_escape_string($bd, trim($_POST['password1']));
-        $password2=mysqli_real_escape_string($bd, trim($_POST['password2']));
-        if(!empty($username) && !empty($password1) && !empty($password2) && ($password1 == $password2))   {
-        $query = "SELECT * FROM 'signup' WHERE username = '$username'";
-        $data = mysqli_query($bd, $query);
-        if(mysqli_num_rows($data) == 0) {
-            $query="INSERT INTO 'signup' (username, password) VALUES ('$username', '$password1')";
-mysqli_query($bd, $query);
-echo 'Вы зарегистрированы';
-mysqli_close($bd);
-exit();
+$mysql= new mysqli("127.0.0.1", "root", "root", "registration", 3306);
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+    if (!empty($username) && !empty($password1) && !empty($password2) && $password1 == $password2) {
 
+        $mysql->query("SELECT * FROM `signup` WHERE `username`='$username' and `password`='$password2'");
+        $mysql->close();
+        header("Location: test.php");
+        echo 'Вы зарегистрированы';
+        exit();
+
+
+            }
+            else {
+                echo 'Все поля должны быть заполнены';
+            }
         }
-        else {
-        echo 'Логин уже существует';
-        }
-        }
-}
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -32,13 +31,16 @@ exit();
     <title>Question</title>
 </head>
 <body>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"></form>
-<label for="username">Введите Ваш логин</label>
+<form method="post" action="?">
+<label>Введите Ваш логин</label>
 <input type="text" name="username">
 <label for="password">Введите Ваш пароль</label>
-<input type="text" name="password1">
+<input type="password" name="password1">
 <label for="password">Повторите Ваш пароль еще раз</label>
-<input type="text" name="password2">
-<button name="submit">Вход</button>
+<input type="password" name="password2">
+<button name="submit">Вход
+</form>
+<a href=""
+
 </body>
 </html>
