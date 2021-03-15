99f6ece5c23890a466e6eef0c82adf3977407553
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-03-15 15:41:26 +0300

    inserting a table

---
+++ test.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Выявление вкусов потребителей</h1>
+    <p>Здравствуйте, потратьте, пожалуйста, несколько минут своего времени на заполнение следующей анкеты.
+   <form action="" method="post">
+    <h2>Оцените, пожалуйства, приведенные аспекты в отношении к выбору сырков творожных торговой марки "Ми-Шу!" по важности:</h2>
+    <table>
+    <tr>
+        <th>Оценка/Баллы</th>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+</tr>
+<tr>
+    <td>1.Качество</td>
+   <td><input type="radio" name="N1" value="1"></td>
+   <td><input type="radio" name="N1" value="2"></td>
+   <td><input type="radio" name="N1" value="3"></td>
+   <td><input type="radio" name="N1" value="4"></td>
+   <td><input type="radio" name="N1" value="5"></td>
+   </tr>
+   <tr>
+<td>2.Цена</td>
+<td><input type="radio" name="N2" value="1"></td>
+<td><input type="radio" name="N2" value="2"></td>
+<td><input type="radio" name="N2" value="3"></td>
+<td><input type="radio" name="N2" value="4"></td>
+<td><input type="radio" name="N2" value="5"></td>
+</tr>
+<tr>
+<td>3.Бренд</td>
+<td><input type="radio" name="N3" value="1"></td>
+<td><input type="radio" name="N3" value="2"></td>
+<td><input type="radio" name="N3" value="3"></td>
+<td><input type="radio" name="N3" value="4"></td>
+<td><input type="radio" name="N3" value="5"></td>
+</tr>
+<tr>
+<td>4.Известность</td>
+<td><input type="radio" name="N4" value="1"></td>
+<td><input type="radio" name="N4" value="2"></td>
+<td><input type="radio" name="N4" value="3"></td>
+<td><input type="radio" name="N4" value="4"></td>
+<td><input type="radio" name="N4" value="5"></td>
+</tr>
+</table>
+<input type="submt" value="Оценить">
+   </form>
+   <?php 
+   ?>
+</body>
+</html>
